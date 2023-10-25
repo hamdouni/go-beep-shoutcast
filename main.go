d9cf2738c76368fbf3e72974a5a09d21cb20c0e4
Brahim Hamdouni <brahim@hamdouni.com>
2023-10-25 09:53:32 +0200

    without shoutcast

--- main.go
+++ main.go
@@ -1,39 +1,46 @@
 package main
 
 import (
-	"log"
+	"net/http"
 
 	"github.com/faiface/beep"
 	"github.com/faiface/beep/mp3"
 	"github.com/faiface/beep/speaker"
-	"github.com/romantomjak/shoutcast"
 )
 
 func main() {
-	stream, err := shoutcast.Open("http://stream.radioparadise.com/mp3-128")
+	// stream, err := shoutcast.Open("http://stream.radioparadise.com/mp3-128")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	//
+	// // Callback function to be called when song changes
+	// stream.MetadataCallbackFunc = func(m *shoutcast.Metadata) {
+	// 	println("Now listening to: ", m.StreamTitle)
+	// }
+	streamURL := "http://stream.radioparadise.com/mp3-192"
+	resp, err := http.Get(streamURL)
 	if err != nil {
 		panic(err)
 	}
-
-	// Callback function to be called when song changes
-	stream.MetadataCallbackFunc = func(m *shoutcast.Metadata) {
-		println("Now listening to: ", m.StreamTitle)
-	}
+	stream := resp.Body
+	defer stream.Close()
 
 	// Decode audio
 	streamer, format, err := mp3.Decode(stream)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	defer streamer.Close()
 
 	// Initialize speaker
 	err = speaker.Init(format.SampleRate, 8192)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
 	// Play audio
+	println("playing ", streamURL)
 	done := make(chan bool)
 	speaker.Play(beep.Seq(streamer, beep.Callback(func() {
 		done <- true
