b6fadd6a08cf83c82cf546d870d1dc46e42f61c5
Brahim Hamdouni <brahim@hamdouni.com>
2023-11-23 22:05:40 +0100

    wip: http server

--- main.go
+++ main.go
@@ -1,15 +1,39 @@
 package main
 
-import "go-beep-shoutcast/stream"
+import (
+	"log"
+	"net/http"
+
+	"go-beep-shoutcast/stream"
+)
+
+var (
+	err   error
+	count = 0
+	audio struct {
+		teardown func()
+	}
+)
 
 func main() {
-	done := make(chan bool)
-	teardown, err := stream.Play("http://stream.radioparadise.com/mp3-192")
+	http.HandleFunc("/start", start)
+	http.HandleFunc("/stop", stop)
+	http.ListenAndServe(":7334", nil)
+}
+
+func start(w http.ResponseWriter, r *http.Request) {
+	count++
+	audio.teardown, err = stream.Play("http://stream.radioparadise.com/mp3-192")
 	if err != nil {
 		panic(err)
 	}
-	defer teardown()
+	log.Printf("teardown: %#v\n", count)
+}
 
-	// Wait audio to finish playing
-	<-done
+func stop(w http.ResponseWriter, r *http.Request) {
+	if audio.teardown != nil {
+		audio.teardown()
+		return
+	}
+	log.Printf("teardown: %#v\n", count)
 }
